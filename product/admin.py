09a637e0eb6a3950670f19eb7a75b4b72ec5cea2
Rubayedprodhan <rubayedprodhan550@gmail.com>
2026-05-15 01:36:16 +0600

    added

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+#admin.site.register(Product)
+admin.site.register(Product_Review)
+admin.site.register(Our_Brands)
+admin.site.register(Category)
+admin.site.register(Product_Flag)
+admin.site.register(Featured_Categories)
+
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    # অটো-স্লাগ এর জন্য এটি যোগ করুন
+    prepopulated_fields = {'product_slug': ('name',)}
--- product/models.py
+++ product/models.py
@@ -1,110 +1,95 @@
 from django.db import models
-
-# Create your models here.
-class Our_Brands(models.Model): # Our_  Brands
+from django.utils.text import slugify
+class Category(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
+
+
+    def __str__(self):
+        return self.name
+
+class Product_Flag(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Media/images/product_flag')
+
+    def __str__(self):
+        return self.name
+
+class Our_Brands(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Media/images/brand')
+    Description = models.TextField()
 
     def __str__(self):
         return self.name
 
 class Featured_Categories(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-
-
-class Top_Selling_Products(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.IntegerField(max_digits=10, decimal_places=2)
-    Brand = models.CharField(max_length=100)
-    Quantity = models.IntegerField()
-    Description = models.CharField(max_length=100)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    brands =
-     # অফার দাম (৳২,২০০)
+    image = models.ImageField(upload_to='Media/images/featured')
 
     def __str__(self):
         return self.name
 
-class All_Natural(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.IntegerField(max_digits=10, decimal_places=2)
-    Brand = models.CharField(max_length=100)
-    Quantity = models.IntegerField()
-    Description = models.CharField(max_length=100)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+class Product(models.Model):
+  
+    SECTION_CHOICES = [
+        ('top_selling', 'Top Selling Products'),
+        ('all_natural', 'All Natural'),
+        ('exclusive_combo', 'Exclusive Combo Deals'),
+        ('premium_dates', 'Premium Dates'),
+        ('cooking_essentials', 'Cooking Essentials'),
+        ('organic_certified', 'Organic Certified'),
+        ('just_for_you', 'Just For You'),
+    ]
     
-class Exclusive_Combo_Deals(models.Model):
+    section = models.CharField(max_length=30, choices=SECTION_CHOICES, default='just_for_you')
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.IntegerField(max_digits=10, decimal_places=2)
-    Brand = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Media/images/product')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     Quantity = models.IntegerField()
-    Description = models.CharField(max_length=100)
+    Description = models.TextField() 
     discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
     
-class Premium_Dates(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.IntegerField(max_digits=10, decimal_places=2)
-    Brand = models.CharField(max_length=100)
-    Quantity = models.IntegerField()
-    Description = models.CharField(max_length=100)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+   
+    brands = models.ForeignKey(Our_Brands, on_delete=models.CASCADE, null=True, blank=True)
+    catagory = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    flag = models.ForeignKey(Product_Flag, on_delete=models.CASCADE, null=True, blank=True)
+    Featured_Categories = models.ForeignKey(Featured_Categories, on_delete=models.CASCADE, null=True, blank=True)
+  
+    product_slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
+  #  product_id = models.AutoField(primary_key=True)
+
+    def save(self, *args, **kwargs):
+        if not self.product_slug:
+            self.product_slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
+
+   
+
+    def save(self, *args, **kwargs):
+        if not self.product_slug:
+            self.product_slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
+
 
     def __str__(self):
-        return self.name
-    
+        return f"{self.name} ({self.get_section_display()})"
 
-class Cooking_Essentials(models.Model):
+
+class Product_Review(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.IntegerField(max_digits=10, decimal_places=2)
-    Brand = models.CharField(max_length=100)
-    Quantity = models.IntegerField()
-    Description = models.CharField(max_length=100)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
 
     def __str__(self):
-        return self.name
+        return f"Review by {self.name} on {self.product.name}"
     
 
-class Organic_Certified(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.IntegerField(max_digits=10, decimal_places=2)
-    Brand = models.CharField(max_length=100)
-    Quantity = models.IntegerField()
-    Description = models.CharField(max_length=100)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='Media/images/product_gallery')
 
     def __str__(self):
-        return self.name
-    
-
-class Just_For_You(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
-    price = models.IntegerField(max_digits=10, decimal_places=2)
-    Brand = models.CharField(max_length=100)
-    Quantity = models.IntegerField()
-    Description = models.CharField(max_length=100)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-    
-
-
-
-
-
-
+        return f"{self.product.name} Gallery Image"
